1f34ce317237baa8d1bdcd00b444a857516193dc
Văn Tiến <lvtien333@gmail.com>
2024-07-11 12:20:28 +0700

    second commit

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+    public bool isAttacking; // Biến để theo dõi trạng thái tấn công
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("attack");
+        isAttacking = true;
+        StartCoroutine(ResetAttack());
+    }
+
+    public bool IsAttacking()
+    {
+        return isAttacking;
+    }
+
+    // Reset trạng thái tấn công sau một khoảng thời gian
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(5); // Thời gian tấn công (1 giây)
+        isAttacking = false;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,33 +1,98 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Parameter")]
     [SerializeField] private float speed;
     [SerializeField] private float jump;
-    [SerializeField] private bool isGrounded = false;
+
+    [Header("Multiple Jump")]
+    [SerializeField] private int extraJumps;
+    private int jumpCounter;
+
+    [SerializeField] private LayerMask groundLayer;
+    private float horizontalInput;
     private Rigidbody2D rb;
-    private Animator anim;
-    private void Awake()
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+    private PlayerAttack playerAttack;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        playerAttack = GetComponent<PlayerAttack>();
+        jumpCounter = extraJumps; // Khởi tạo jumpCounter với giá trị extraJumps
     }
 
-    // Update is called once per frame
+    // Update được gọi mỗi khung hình
     private void Update()
     {
+        horizontalInput = Input.GetAxisRaw("Horizontal"); // Lấy giá trị đầu vào ngang
+
+        // Di chuyển nhân vật
         Move();
+
+        // Xử lý nhảy
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+            
+            
+        }
+
+        // Xoay nhân vật khi di chuyển
+        Flip();
+
+        // Thiết lập animation
+        animator.SetBool("run", horizontalInput != 0);
+        animator.SetBool("grounded", IsGrounded());
     }
+
+    // Hàm di chuyển
     void Move()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        // Di chuyen nhan vat
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-            Jump();
-        // Xoay nhan vat khi di chuyen
+        float currentSpeed = speed;
+
+        // Nếu nhân vật đang tấn công, giảm tốc độ di chuyển xuống 1/3
+        if (playerAttack != null && playerAttack.IsAttacking())
+        {
+            currentSpeed /= 3;
+        }
+
+        rb.velocity = new Vector2(horizontalInput * currentSpeed, rb.velocity.y);
+    }
+
+    // Hàm nhảy
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            jumpCounter = extraJumps; // Reset jumpCounter khi chạm đất
+            
+
+        }
+        else if (jumpCounter > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            jumpCounter--; // Giảm jumpCounter khi thực hiện nhảy đôi
+
+        }
+        else
+        {
+            return;
+        }
+        animator.SetTrigger("jump");
+
+    }
+
+    // Xoay nhân vật khi di chuyển
+    private void Flip()
+    {
         if (horizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -36,23 +101,12 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        // Animation
-        anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", isGrounded);
-
-    }
-    void Jump()
-    {
-
-        rb.velocity = new Vector2(rb.velocity.x, jump);
-        anim.SetTrigger("jump");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Tạo một lazer box thẳng xuống kiểm tra nhân vật có va chạm với ground không
+    private bool IsGrounded()
     {
-        if (collision.gameObject.tag == "ground")
-        {
-            isGrounded = true;
-        }
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
