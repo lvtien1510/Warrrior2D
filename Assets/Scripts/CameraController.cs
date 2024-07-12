85e334bb0c2018ea3d8134d5f598c7f825eca90f
Văn Tiến <lvtien333@gmail.com>
2024-07-13 02:55:05 +0700

    di chuyen va camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Player Settings")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+
+    [Header("Camera Limits")]
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
+    private float lookAhead;
+    private Vector3 targetPosition;
+
+    private void LateUpdate()
+    {
+        // Xác định vị trí mục tiêu
+        transform.position = new Vector3(player.position.x + lookAhead, player.position.y, transform.position.z);
+
+        // Làm mượt chuyển động của camera
+        targetPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+
+        // Áp dụng giới hạn cho vị trí của camera
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        // Cập nhật vị trí camera
+        transform.position = targetPosition;
+
+        // Cập nhật lookAheadX và lookAheadY
+        lookAhead = Mathf.Lerp(lookAhead, aheadDistance * player.localScale.x, Time.deltaTime * cameraSpeed);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,15 +16,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
     private BoxCollider2D boxCollider;
-    private PlayerAttack playerAttack;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
-        playerAttack = GetComponent<PlayerAttack>();
-        jumpCounter = extraJumps; // Khởi tạo jumpCounter với giá trị extraJumps
+        jumpCounter = extraJumps;
     }
 
     // Update được gọi mỗi khung hình
@@ -34,60 +31,34 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal"); // Lấy giá trị đầu vào ngang
 
         // Di chuyển nhân vật
-        Move();
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        animator.SetBool("run", horizontalInput != 0);
+        bool isFalling = rb.velocity.y <= 0.1f && !IsGrounded(); 
+        animator.SetBool("jumpdown", isFalling);
 
         // Xử lý nhảy
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
-            
+            animator.SetTrigger("jumpup");
+            if (IsGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+                jumpCounter = extraJumps; // Reset jumpCounter khi chạm đất
+                
+            }
+            else if (jumpCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+                jumpCounter--; // Giảm jumpCounter khi thực hiện nhảy đôi
+            }
+            else return;
             
         }
 
         // Xoay nhân vật khi di chuyển
         Flip();
-
-        // Thiết lập animation
-        animator.SetBool("run", horizontalInput != 0);
-        animator.SetBool("grounded", IsGrounded());
-    }
-
-    // Hàm di chuyển
-    void Move()
-    {
-        float currentSpeed = speed;
-
-        // Nếu nhân vật đang tấn công, giảm tốc độ di chuyển xuống 1/3
-        if (playerAttack != null && playerAttack.IsAttacking())
-        {
-            currentSpeed /= 3;
-        }
-
-        rb.velocity = new Vector2(horizontalInput * currentSpeed, rb.velocity.y);
-    }
-
-    // Hàm nhảy
-    void Jump()
-    {
-        if (IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jump);
-            jumpCounter = extraJumps; // Reset jumpCounter khi chạm đất
-            
-
-        }
-        else if (jumpCounter > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jump);
-            jumpCounter--; // Giảm jumpCounter khi thực hiện nhảy đôi
-
-        }
-        else
-        {
-            return;
-        }
-        animator.SetTrigger("jump");
-
+        
     }
 
     // Xoay nhân vật khi di chuyển
@@ -106,7 +77,7 @@ public class PlayerMovement : MonoBehaviour
     // Tạo một lazer box thẳng xuống kiểm tra nhân vật có va chạm với ground không
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.2f, groundLayer);
         return raycastHit.collider != null;
     }
 }
